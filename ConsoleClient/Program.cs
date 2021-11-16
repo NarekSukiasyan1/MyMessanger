c127861b9556c482e11e926f6cc5f5d119ccb7bb
Нарек Сукиасян <narek.sukiasyan.03@mail.ru>
2021-11-16 22:25:01 +0300

    Add ASP.NET

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace MyMessanger {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Message msg = new Message("Admin","Hello Me",DateTime.Now);
+            Console.WriteLine(msg);
+            String output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message newmsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(newmsg);
         }
     }
 }

6310a8745101c2c7158b2042085f4b7106f689c5
Нарек Сукиасян <narek.sukiasyan.03@mail.ru>
2021-11-17 20:09:48 +0300

    Created Console Aplication

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Timers;
 namespace MyMessanger {
     class Program {
-        static void Main(string[] args) {
-            Message msg = new Message("Admin","Hello Me",DateTime.Now);
-            Console.WriteLine(msg);
-            String output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message newmsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(newmsg);
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages() {
+            while (true) {
+                Message msg = API.GetMessage(MessageID);
+                while (msg != null) {
+                    Console.WriteLine(msg);
+                    MessageID++;
+                    msg = API.GetMessage(MessageID);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main() {
+            MessageID = 0;
+            Console.WriteLine("Введите Ваше имя:");
+            //UserName = "RusAl";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            Thread myThread = new Thread(new ThreadStart(GetNewMessages));
+            myThread.Start(); // запускаем поток
+            while (MessageText != "exit") {
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1) {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+
+            }
         }
     }
 }
